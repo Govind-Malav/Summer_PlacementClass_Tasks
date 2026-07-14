6f6034b811eb7b052b7dcb1d7edaca3890b86bf2
Govind <govindmalav2004@gmail.com>
2026-07-14 14:38:19 +0530

    Tic tac toe game

---
+++ pattern1.py
@@ -0,0 +1,21 @@
+class pattern:
+    def print_star(n):
+        for i in range(n):
+            print(" "*(n-i-1)+"*"*(2*i+1))
+        for i in range(n - 2, -1, -1):
+            print(" " * (n - i - 1) + "*" * (2 * i + 1))
+    print_star(4)
+    #     for i in range(1,n+1):
+    #         for j in range(n-i):
+    #             print(" ",end=" ")
+    #         for k in range(2*i -1):
+    #             print("*",end=" ")
+    #         for j in range(n-i):
+    #             print(" ",end=" ")
+    #         print()    
+    # print_star(4)
+    for i in range(n):
+        print(" "*())
+    for i in range(n - 1, -1, -1):
+         print(" " * (n - i - 1) + "*" * (2 * i + 1))
+          
